f0e6358a595753069207fdfc723dc9a35cec4119
Lachezar Dimitrov <lachezar_dimitrov@outlook.com>
2025-01-03 22:52:04 +0200

    feat: add index html from the hyper blog project

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const input = require('sync-input');
+const input = require("sync-input");
 
 const itemPrices = [
-    {name: "Bubblegum", price: 202},
-    {name: "Toffee", price: 118},
-    {name: "Ice Cream", price: 2250},
-    {name: "Milk Chocolate", price: 1680},
-    {name: "Doughnut", price: 1075},
-    {name: "Pancake", price: 80}
-]
+    { name: "Bubblegum", price: 202 },
+    { name: "Toffee", price: 118 },
+    { name: "Ice Cream", price: 2250 },
+    { name: "Milk Chocolate", price: 1680 },
+    { name: "Doughnut", price: 1075 },
+    { name: "Pancake", price: 80 },
+];
 
 function logItemPrices(itemPrices) {
-    itemPrices.forEach(item => {
+    itemPrices.forEach((item) => {
         console.log(`${item.name}: $${item.price}`);
     });
 }
@@ -27,13 +27,10 @@ console.log("Earned amount:");
 
 logItemPrices(itemPrices);
 
-console.log('\n');
+console.log("\n");
 console.log(`Income: $${calculateIncome(itemPrices)}`);
 
 const staffExpenses = Number(input("Staff expenses:"));
 const otherExpenses = Number(input("Other expenses:"));
 
 console.log(`Net income: $${calculateNetIncome(itemPrices, staffExpenses, otherExpenses)}`);
-
-
-
